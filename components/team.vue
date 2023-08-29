7f2ae134baf46393f53ca127e16df503e4aa8d5c
Oscar-roqsoft <oscar.nnoje@gmail.com>
2023-08-29 15:38:29 +0100

    hh

--- components/team.vue
+++ components/team.vue
@@ -5,7 +5,8 @@
             <h1 class="text-gray-950 font-extrabold text-lg my-1">Meet My Expert Team</h1>
             <div
             class="grid grid-cols-1  md:grid-cols-3 gap-3 md:gap-5 mt-6">
-                <div  v-for="item in teams"
+                <div  v-animate-on-scroll
+                v-for="item in teams"
                 class="w-[300px] md:w-[200px] bg-gray-900 rounded">
                     <div>
                         <img :src="item.img" class="w-full">
@@ -40,3 +41,17 @@ const teams =[
     }
 ]
 </script>
+
+
+<style scoped>
+
+.before-enter{
+    opacity: 0;
+    transform: translateY(100px);
+    transition: all 2s ease-in-out ;
+}
+.enter{
+    opacity: 1;
+    transform: translateY(0);
+}
+</style>
